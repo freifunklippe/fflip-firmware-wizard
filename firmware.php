03a0a807323fdac6d30c4e09c07ca99883fa4291
Freifunk Lippe <kontakt@freifunk-lippe.de>
2017-09-29 06:26:40 +0200

    Update firmware.php

--- firmware.php
+++ firmware.php
@@ -416,7 +416,7 @@ $community_id = $_REQUEST['id'];
 
             <footer>
                 <div class="col-xs-12 text-muted text-center">
-                    <p>Licensed under GPLv3 / Copyright 2016 by Caspar Armster, <a href="http://www.freifunk-hennef.de/">Freifunk Hennef</a> / <a href="http://www.freie-netzwerker.de/">Freie Netzwerker e.V.</a></p>
+                    <p>Licensed under GPLv3 / Copyright 2016 by Caspar Armster, <a href="http://www.freifunk-hennef.de/">Freifunk Hennef</a> / <a href="http://www.freie-netzwerker.de/">Freie Netzwerker e.V.</a> / <a href="https://freifunk-lippe.de/">Freifunk Lippe</a></p>
                 </div>
             </footer>
         </div>
